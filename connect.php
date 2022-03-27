5226b44aada9ae656262805a47bd62fc782d70ad
Nikithakukunuru <64829933+Nikithakukunuru@users.noreply.github.com>
2022-03-27 21:05:58 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -1,4 +1,18 @@
 <?php
+
+$servername = "localhost";
+$username = "username";
+$password = "password";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+
 $ref_id=$_POST['ref_id'];
 $surname=$_POST['surname'];
 $cand_name=$_POST['cand_name'];
